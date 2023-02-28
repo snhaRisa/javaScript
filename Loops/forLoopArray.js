d613bdb4ae8e234b162a4fd38c968de6b6c788aa
Rishav <risasnha1999@gmail.com>
2023-02-28 15:06:13 +0530

    Add new files

---
+++ Loops/forLoopArray.js
@@ -0,0 +1,9 @@
+//For loop in an array; 
+
+const fruits = ['Apple', 'Mango', 'Banana'];
+let count=0; 
+
+for(count; count<fruits.length; count++)
+{
+    console.log(fruits[count]);
+}
---
+++ Loops/forLoopString.js
@@ -0,0 +1,9 @@
+//For loop for string. 
+
+const val = 'Shaily';
+let count = 0 ; 
+
+for(count; count<val.length; count++)
+{
+    console.log(val.charAt(count));
+}
